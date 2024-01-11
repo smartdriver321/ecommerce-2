9302ba67985ec2fac90029f1e1e82910fda91102
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-01-11 19:23:12 +0700

    Add Categories component and Filter provider

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -20,8 +20,8 @@ import { generateMeta } from '../../_utilities/generateMeta'
 // If you are not using Payload Cloud then this line can be removed, see `../../../README.md#cache`
 export const dynamic = 'force-dynamic'
 
-//import Categories from '../../_components/Categories'
 //import Promotion from '../../_components/Promotion'
+import Categories from '../../_components/Categories'
 
 import classes from './index.module.scss'
 
@@ -66,8 +66,7 @@ export default async function Page({ params: { slug = 'home' } }) {
           <Hero {...hero} />
 
           <Gutter className={classes.home}>
-            {/* <Categories categories={categories} />
-            <Promotion /> */}
+            <Categories categories={categories} />
           </Gutter>
         </section>
       ) : (
